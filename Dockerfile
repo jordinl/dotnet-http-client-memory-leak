8424decbe6afe348a3981e55415c1ec6cc12082b
Jordi Noguera <jordinoguera83@gmail.com>
2024-10-09 16:00:42 +0200

    First commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:8.0 AS base
+USER $APP_UID
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["dotnet-http-client-memory-leak.csproj", "./"]
+RUN dotnet restore "dotnet-http-client-memory-leak.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "dotnet-http-client-memory-leak.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "dotnet-http-client-memory-leak.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "dotnet-http-client-memory-leak.dll"]
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    private static readonly HttpClient HttpClient = new();
+    private static readonly int Concurrency = int.Parse(Environment.GetEnvironmentVariable("CONCURRENCY") ?? "20");
+    private static readonly int Limit = int.Parse(Environment.GetEnvironmentVariable("LIMIT") ?? "1000");
+
+    static async Task Main()
+    {
+        Console.WriteLine("Starting:");
+        Console.WriteLine($" * Concurrency: {Concurrency}");
+        Console.WriteLine($" * Limit: {Limit}");
+
+        HttpClient.Timeout = TimeSpan.FromSeconds(5);
+
+        var start = DateTime.Now;
+        var urls = File.ReadLines("urls.txt").Take(Limit);
+        var semaphore = new SemaphoreSlim(Concurrency, Concurrency);
+
+        var tasks = urls.Select(async url =>
+        {
+            await semaphore.WaitAsync();
+            string code;
+
+            try
+            {
+                using var response = await HttpClient.GetAsync(url);
+                code = ((int)response.StatusCode).ToString();
+            }
+            catch (Exception ex)
+            {
+                code = (ex.InnerException ?? ex).GetType().Name;
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+
+            Console.WriteLine($"{url}: {code}");
+        });
+
+        await Task.WhenAll(tasks);
+        Console.WriteLine($"Finished: {DateTime.Now - start}");
+    }
+}
